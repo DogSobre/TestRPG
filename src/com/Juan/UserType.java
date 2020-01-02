ef3944a78695be367e2010652070adca822dbb28
Juan <juanfernandoherrada13@gmail.com>
2020-01-02 15:30:32 +0100

    Test from create RPG CLASS
    In this commit, trying to implement the berserker class into the UserType.java

--- src/com/Juan/UserType.java
+++ src/com/Juan/UserType.java
@@ -4,6 +4,9 @@ import java.util.Scanner;
 
 public class UserType{
 
+    public UserType() {
+    }
+
     public static String getUserType(){
 
         String str = null;
@@ -12,58 +15,62 @@ public class UserType{
         String finalUserType = "";
         String strLower = str.toLowerCase();
 
-        if(strLower.equals("berserker")){
+        public String Berserker(){
 
-            do{
+            if (strLower.equals("berserker")) {
 
-                System.out.println("You choose Berserker\n" + "Please now, choose a name");
-                Scanner userType = new Scanner(System.in);
-                str = userType.nextLine();
-                String tempUserType = str;
+                do {
 
-                System.out.println("You choose '" + str + "', is it correct ? (Y/N) \n");
-                userType = new Scanner(System.in);
-                str = userType.nextLine().toLowerCase();
+                    System.out.println("You choose Berserker\n" + "Please now, choose a name");
+                    Scanner userType = new Scanner(System.in);
+                    str = userType.nextLine();
+                    String tempUserType = str;
 
-                if(str.equals("y")){
+                    System.out.println("You choose '" + str + "', is it correct ? (Y/N) \n");
+                    userType = new Scanner(System.in);
+                    str = userType.nextLine().toLowerCase();
 
-                    finalUserType = tempUserType;
-                    System.out.println("Ok, now see the different characteristics of the Berserker :");
-                    MasterClass berserker = new MasterClass(467, 81, 0, 37, 29, 315, 0.89);
+                    if (str.equals("y")) {
 
-                    System.out.println(berserker);
-                    System.out.println("To continue, please type 'ok'");
-                    isUserChosen = true;
-                }
-            }while (!isUserChosen);
+                        finalUserType = tempUserType;
+                        System.out.println("Ok, now see the different characteristics of the Berserker :");
+                        MasterClass berserker = new MasterClass(467, 81, 0, 37, 29, 315, 0.89);
+
+                        System.out.println(berserker);
+                        System.out.println("To continue, please type 'ok'");
+                        isUserChosen = true;
+                    }
+                } while (!isUserChosen);
+            }
             return str;
         }
-//        else if (strLower.equals("thief")){
-//
-//            do {
-//
-//                System.out.println("You choose Thief\n" + "Please now, choose a name");
-//                Scanner userType = new Scanner(System.in);
-//                str = userType.nextLine();
-//                String tempUserType = str;
-//
-//                System.out.println("You choose '" + str + "', is it correct ? (Y/N) \n");
-//                userType = new Scanner(System.in);
-//                str = userType.nextLine().toLowerCase();
-//
-//                if (str.equals("y")) {
-//
-//                    finalUserType = tempUserType;
-//                    System.out.println("Ok, now see the different characteristics of the Thief :");
-//                    MasterClass berserker = new MasterClass(329, 57, 23, 10, 10, 425, 1.02);
-//
-//                    System.out.println(berserker);
-//                    System.out.println("To continue, please type 'ok'");
-//                    isUserChosen = true;
-//                }
-//            }while (!isUserChosen);
-//            return str;
-//        }
-        return finalUserType;
+        public String Thief(){
+            if (strLower.equals("thief")) {
+
+                do {
+
+                    System.out.println("You choose Thief\n" + "Please now, choose a name");
+                    Scanner userType = new Scanner(System.in);
+                    str = userType.nextLine();
+                    String tempUserType = str;
+
+                    System.out.println("You choose '" + str + "', is it correct ? (Y/N) \n");
+                    userType = new Scanner(System.in);
+                    str = userType.nextLine().toLowerCase();
+
+                    if (str.equals("y")) {
+
+                        finalUserType = tempUserType;
+                        System.out.println("Ok, now see the different characteristics of the Thief :");
+                        MasterClass berserker = new MasterClass(329, 57, 23, 10, 10, 425, 1.02);
+
+                        System.out.println(berserker);
+                        System.out.println("To continue, please type 'ok'");
+                        isUserChosen = true;
+                    }
+                }while(!isUserChosen);
+            }
+            return str;
+        }
     }
 }
