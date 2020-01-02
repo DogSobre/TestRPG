4f13bfe7a9be37c05ecde516af17c20df6d015b7
Juan <juanfernandoherrada13@gmail.com>
2020-01-02 15:30:32 +0100

    Test from "create" RPG CLASS
    In this commit, trying to implement the berserker class into the UserType.java

--- src/com/Juan/Main.java
+++ src/com/Juan/Main.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args){
 	// write your code here
         System.out.println("\nWelcome to the new rpg from DogSobre's Industries\n");
         String help = "- help\n" + "- create\n" + "- exit";
@@ -24,52 +24,54 @@ public class Main {
                     break;
                 case "create":
                     System.out.println("Choose a class :\n" + rpgClass + "If you are done, please write 'ok'\n");
+                    UserType create = new UserType();
+                    System.out.println(create);
 
-                    do{
-                        userType = new Scanner(System.in);
-                        str = userType.nextLine();
-
-                        ////// In first place, the user choose his class.
-                        ////// In second place, the user choose a username.
-                        ////// After that, the program ask to the user if he confirm the username. He choice Yes (Y, y) or no (N, n).
-                        ////// If the user say Yes, the script continue into the loop until the end and break it when finished.
-                        ////// If the user say No, the loop restart at the moment when the program asked to user what is his username.
-
-                        if(str.equals("Berserker") || str.equals("berserker")){
-                            try{
-                                String berserk = new String();
-                                berserk = UserType.getUserType();
-                                System.out.println(berserk);
-                            }catch (Exception e){
-                                System.out.println("Process restarting.\n" + "Please choose a class :\n" + rpgClass);
-                            }
-                        }else if (str.equals("Thief") || str.equals("thief")){
+//                    do{
+//                        userType = new Scanner(System.in);
+//                        str = userType.nextLine();
+//
+//                        ////// In first place, the user choose his class.
+//                        ////// In second place, the user choose a username.
+//                        ////// After that, the program ask to the user if he confirm the username. He choice Yes (Y, y) or no (N, n).
+//                        ////// If the user say Yes, the script continue into the loop until the end and break it when finished.
+//                        ////// If the user say No, the loop restart at the moment when the program asked to user what is his username.
+//
+//                        if(str.equals("Berserker") || str.equals("berserker")){
 //                            try{
-//                                String thief = new String();
-//                                thief = UserType.getUserType();
-//                                System.out.println(thief);
-//                            }catch(Exception e){
+//                                String newUserType = UserType.getUserType();
+//                                UserType berserk = new UserType();
+//                                System.out.println(berserk);
+//                            }catch (Exception e){
 //                                System.out.println("Process restarting.\n" + "Please choose a class :\n" + rpgClass);
 //                            }
-                            do{
-                                System.out.println("You choose Thief\n" + "Please now, choose a name");
-                                userType = new Scanner(System.in);
-                                str = userType.nextLine();
-
-                                System.out.println("You choose '" + str + "', is it correct ? (Y/N) \n");
-                                userType = new Scanner(System.in);
-                                str = userType.nextLine();
-                                if(str.equals("Y") || str.equals("y")){
-                                    System.out.println("Ok, now see the different characteristics of the Thief :");
-                                    MasterClass thief = new MasterClass(329, 57, 23, 10, 10, 425, 1.02);
-
-                                    System.out.println(thief);
-                                    System.out.println("To continue, please type 'ok'");
-                                    break;
-                                }
-                            }while(str.equals("N") != true || str.equals("n") != true);
-                        }
-                    }while(str.equals("ok") != true);
+//                        }else if(str.equals("Thief") || str.equals("thief")){
+////                            try{
+////                                String thief = new String();
+////                                thief = UserType.getUserType();
+////                                System.out.println(thief);
+////                            }catch(Exception e){
+////                                System.out.println("Process restarting.\n" + "Please choose a class :\n" + rpgClass);
+////                            }
+//                            do{
+//                                System.out.println("You choose Thief\n" + "Please now, choose a name");
+//                                userType = new Scanner(System.in);
+//                                str = userType.nextLine();
+//
+//                                System.out.println("You choose '" + str + "', is it correct ? (Y/N) \n");
+//                                userType = new Scanner(System.in);
+//                                str = userType.nextLine();
+//                                if(str.equals("Y") || str.equals("y")){
+//                                    System.out.println("Ok, now see the different characteristics of the Thief :");
+//                                    MasterClass thief = new MasterClass(329, 57, 23, 10, 10, 425, 1.02);
+//
+//                                    System.out.println(thief);
+//                                    System.out.println("To continue, please type 'ok'");
+//                                    break;
+//                                }
+//                            }while(str.equals("N") != true || str.equals("n") != true);
+//                        }
+//                    }while(str.equals("ok") != true);
                     break;
 
                 /*case(str.equals("fight")){
